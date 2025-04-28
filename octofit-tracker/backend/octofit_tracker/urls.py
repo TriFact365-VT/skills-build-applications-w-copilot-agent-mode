8b6db8f47cdd501f091c8bdf8721708715e03c33
TriFact365-VT <47520547+TriFact365-VT@users.noreply.github.com>
2025-04-28 11:54:33 +0000

    Update URLs configuration for OctoFit API endpoints

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import include
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, TeamViewSet, ActivityViewSet, LeaderboardViewSet, WorkoutViewSet, api_root
 
